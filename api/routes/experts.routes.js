d991270155646c6f818f8e202e49cd8082af6688
Ayoola Solomon <ayoola.solomon@andela.co>
2015-04-18 14:30:27 +0100

    Send experts to Hubot

--- api/routes/experts.routes.js
+++ api/routes/experts.routes.js
@@ -1,15 +1,16 @@
 var Firebase = require('firebase'),
-    needle = require('needle'),
-    _ = require('lodash');
+    needle 	 = require('needle'),
+    _ 		 = require('lodash')
+    request  = require('request');
 
 module.exports = function(app, config) {
     var root = new Firebase(config.firebase.rootRefUrl);
 
-    app.route('/experts').get(function(req, res) {
+    app.route('/experts').get(function(req, res, next) {
         root.child('users').on('value', function(snap) {
-            var data = snap.val(),
+            var data   = snap.val(),
                 skills = {},
-                match = [];
+                expertMatch  = {};
 
             // Waiting to get tags from Seun Martins to help in filtering experts
             var tagsFromQuestion = ['Javascript', 'CSS', 'PHP', 'Java', 'Scala'];
@@ -18,8 +19,16 @@ module.exports = function(app, config) {
                 if (data.hasOwnProperty(i)) {
                     var tagsMatchSkill = _.intersection(data[i].skills, tagsFromQuestion);
                     if (tagsMatchSkill.length > 0) {
-                        res.json({
-                            matches: data[i].slack
+                        expertMatch['user'] = data[i];
+                        send(expertMatch, function(error, status, body) {
+                            if (error) {
+                              return next(error);
+                            } else if (status !== 200) {
+                              // inform user that our Incoming WebHook failed
+                              return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+                            } else {
+                              return res.status(200).end();
+                            }
                         });
                     }
                 }
@@ -28,3 +37,24 @@ module.exports = function(app, config) {
         });
     });
 };
+
+
+function send (payload, cb) {
+  console.log('payload: ', payload);
+  var uri = ' https://yodabot.herokuapp.com/experts';
+ 
+  request({
+    uri: uri,
+    method: 'POST',
+    json: payload
+  }, function (error, response, body) {
+    console.log('response: ', response.statusCode);
+    console.log('body: ', body);
+    if (error) {
+      return cb(error);
+    }
+ 
+    cb(null, response.statusCode, body);
+  });
+}
+
