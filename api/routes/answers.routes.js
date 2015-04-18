262d9f8b6c93424fe72c80b011bef65308a595f0
andela-osogunle <sogunleolalekan@gmail.com>
2015-04-18 15:25:33 +0100

    formatted answer yoda

--- api/routes/answers.routes.js
+++ api/routes/answers.routes.js
@@ -4,9 +4,12 @@ module.exports = function(app, config) {
   app.route('/answers').post(function(req, res) {
     var root = new Firebase(config.firebase.rootRefUrl);
     var answer = {};
+    var answer.yoda = {};
     answer.question = req.body.question_id.toString();
     answer.content = req.body.content;
-    answer.yoda    = req.body.user;
+    answer.yoda.name    = req.body.name;
+    answer.yoda.id      = req.body.id;
+    answer.yoda.email   = req.body.email_address;
     answer.created_at = Firebase.ServerValue.TIMESTAMP;
     root.child('questions').child(answer.question).child('answers').push(answer);
     res.json ( answer );
