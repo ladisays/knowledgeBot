9fba2c07f575f3a6311ff677e7680f8eb9c176e3
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2015-04-18 01:20:20 +0100

    Refactoring the Codebase

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -5,20 +5,13 @@ _ = require('lodash');
 module.exports = function(app, config) {
   var root = new Firebase(config.firebase.rootRefUrl);
   app.route('/users/register').post(function(req, res) {
-    var base = 'https://api.github.com';
-    // Get data from the request body
     var data = req.body;
     data.skills = true;
-    // Ensure user does not exist in the firebase
     root.child('users').orderByChild('username')
     .startAt(data.slack).endAt(data.slack)
     .on('value', function(snap) {
       if(snap.val()) {
-        res.json({'error': 'This user already exists'});
-        var user = snap.val();
-        if(user.github === data.github) {
-          res.json({'error': 'You have already signed up'});
-        }
+        res.json({error: 'This user already exists'});
       } else {
         root.child('users').push(data, function(err) {
           if(!err) {
@@ -27,28 +20,6 @@ module.exports = function(app, config) {
         });
       }
     });
-    // needle.get(base + '/users/' + data.github + '/repos', function(error, response) {
-    //   console.log(base + '/users/' + data.github + '/repos');
-    //   var valid = [];
-    //   if (!error && response.statusCode == 200) {
-    //     var repos = response.body;
-    //     console.log(repos);
-    //     _.forEach(repos, function(repo) {
-    //       if(!repo.private && !repo.fork) {
-    //         console.log(repo.name);
-    //         valid.push(repo.name);
-    //       }
-    //     });
-    //     var languages = [];
-    //     _.forEach(valid, function(rep) {
-    //       needle.get(base + '/repos/' + data.github + '/' + rep + '/languages', function(error, lang) {
-    //         if (!error && lang.statusCode == 200) {
-    //           languages = _.union(languages, _.keys(lang.body));
-    //         }
-    //       });
-    //     });
-    //   }
-    // });
   });
   app.route('/users/skills').post(function(req, res) {
     var data = req.body;
@@ -69,7 +40,7 @@ module.exports = function(app, config) {
           }
         });
       } else {
-        res.json({'error': 'This user does not exists'});
+        res.json({error: 'This user does not exists'});
       }
     });
   });
